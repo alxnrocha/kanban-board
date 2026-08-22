eb9bbd97f49493ff0a9e35a7885b4933fc7906d8
Alexandre Rocha <alxnrocha@gmail.com>
2026-08-22 21:24:22 +0200

    revert(ui): completely remove author project badge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { AppLayout } from './components/layout';
 import { KanbanBoard } from './components/kanban';
-import { ProjectBadge } from './components/ProjectBadge';
 
 export const App: React.FC = () => {
   return (
     <AppLayout>
       <KanbanBoard />
-      <ProjectBadge />
     </AppLayout>
   );
 };
